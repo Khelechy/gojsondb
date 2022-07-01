0e30b1bf0fd8f44cf2aad74294f6dff0003ccbcc
khelechy <onyekwerekelechimac@gmail.com>
2022-07-01 12:27:49 +0100

    set up, load

--- main.go
+++ main.go
@@ -4,28 +4,67 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"reflect"
 
 	"encoding/json"
 )
 
-var dbObject interface{}
+type JsonDB struct {
+	jsonObject map[string]interface{}
+}
 
 func main() {
 	fmt.Println("Hello word")
-	Load("user.json")
+	db, err := Load("user.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(db.jsonObject["users"])
 }
 
-func Load(filePathLoaded string) (interface{}, error) {
-	data, err := ioutil.ReadFile(filePathLoaded)
+func Load(filePathLoaded string) (JsonDB, error) {
+	//data, err := ioutil.ReadFile(filePathLoaded)
+	jsonDB := JsonDB{}
+	var dbObject map[string]interface{}
+
+	jsonFile, err := os.Open(filePathLoaded)
+
 	if err != nil {
-		return nil, errors.New("File does not exist, please check file or file path")
+		return jsonDB, errors.New("File does not exist, please check file or file path")
 	}
+
+	defer jsonFile.Close()
 	//Try unmarshal
 
-	err = json.Unmarshal(data, &dbObject)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	err = json.Unmarshal(byteValue, &dbObject)
 	if err != nil {
-		return nil, errors.New("Could not unmarshal json")
+		return jsonDB, errors.New("Could not unmarshal json")
 	}
 
-	return &dbObject, nil
+	returnObject := &JsonDB{
+		jsonObject: dbObject,
+	}
+
+	return *returnObject, nil
+}
+
+func (jsonDB *JsonDB) Select(table string) interface{} {
+	return jsonDB.jsonObject[table]
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func getProp(d interface{}, label string) (interface{}, bool) {
+	switch reflect.TypeOf(d).Kind() {
+	case reflect.Struct:
+		v := reflect.ValueOf(d).FieldByName(label)
+		return v.Interface(), true
+	}
+	return nil, false
 }
