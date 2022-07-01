ad0a17169c8e73ed25dec2dd34e502264db19642
khelechy <onyekwerekelechimac@gmail.com>
2022-07-01 11:11:23 +0100

    added load func

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+
+	"encoding/json"
 )
 
-func main(){
-	fmt.Println(("Hello word"))
+var dbObject interface{}
+
+func main() {
+	fmt.Println("Hello word")
+	Load("user.json")
+}
+
+func Load(filePathLoaded string) (interface{}, error) {
+	data, err := ioutil.ReadFile(filePathLoaded)
+	if err != nil {
+		return nil, errors.New("File does not exist, please check file or file path")
+	}
+	//Try unmarshal
+
+	err = json.Unmarshal(data, &dbObject)
+	if err != nil {
+		return nil, errors.New("Could not unmarshal json")
+	}
+
+	return &dbObject, nil
 }
